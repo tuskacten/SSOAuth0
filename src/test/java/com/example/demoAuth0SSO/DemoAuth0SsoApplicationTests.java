6904081be6a846bb3fccff517fb9ab4e83cd13d7
Nguyen-DucManh <22024504@vnu.edu.vn>
2025-03-11 15:49:36 +0700

    First commit

---
+++ src/test/java/com/example/demoAuth0SSO/DemoAuth0SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAuth0SSO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuth0SsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
